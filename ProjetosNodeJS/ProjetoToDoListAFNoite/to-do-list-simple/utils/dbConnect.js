d2e46c736954487578aec75ea673eaa7cd061c70
ChristopherM51 <123783949+ChristopherM51@users.noreply.github.com>
2024-09-02 21:50:08 -0300

    02.09.24

---
+++ ProjetosNodeJS/ProjetoToDoListAFNoite/to-do-list-simple/utils/dbConnect.js
@@ -0,0 +1,7 @@
+import mongoose from "mongoose";
+
+const connectMongo = async () => {
+    mongoose.connect(process.env.DATABASE_URL)// estabelecendo conexão
+    .then(()=>console.log("Conectado ao MongoDB"))
+    .catch((err) => console.error("Erro ao conectar ao MongoDB",err))
+}
